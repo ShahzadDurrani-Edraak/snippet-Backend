0e54db5eb7775d488509e7a085d12d90fbe08bd3
Shahzad Durrani <Shahzad@Asifas-MacBook-Pro.local>
2023-07-12 12:02:16 +0500

    image upload code fixing

--- server.js
+++ server.js
@@ -14,16 +14,10 @@ const app = express();
 const AWS = require("aws-sdk");
 require("aws-sdk/lib/maintenance_mode_message").suppress = true;
 const s3 = new AWS.S3();
+var sharp = require("sharp");
 
 app.use(cors());
 //app.use("/images", express.static("images"));
-
-// app.use((req, res, next) => {
-//   res.setHeader("Access-Control-Allow-Origin", "https://snip-ad.netlify.app");
-//   res.setHeader("Access-Control-Allow-Methods", "GET, POST");
-//   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-//   next();
-// });
 
 app.use(bodyParser.json());
 //const upload = multer({ dest: path.join(__dirname, "/images") });
@@ -61,6 +55,72 @@ mongoose
     console.log("Failed to connect to MongoDB", err);
   });
 
+var upload = multer({
+  limits: { fileSize: 10 * 1000 * 1000 }, // now allowing user uploads up to 10MB
+  fileFilter: function (req, file, callback) {
+    let fileExtension = file.originalname
+      .split(".")
+      [file.originalname.split(".").length - 1].toLowerCase(); // convert extension to lower case
+    if (["png", "jpg", "jpeg"].indexOf(fileExtension) === -1) {
+      return callback("Wrong file type", false);
+    }
+    file.extension = fileExtension.replace(/jpeg/i, "jpg"); // all jpeg images to end .jpg
+    callback(null, true);
+  },
+  storage: multer.diskStorage({
+    destination: "/images", // store in local filesystem
+    filename: function (req, file, cb) {
+      cb(
+        null,
+        `${req.user._id.toHexString()}-${Date.now().toString()}.${
+          file.extension
+        }`
+      ); // user id + date
+    },
+  }),
+});
+
+app.post("/api/upload", upload.single("file"), function (req, res, next) {
+  const image = sharp(req.file.path); // path to the stored image
+  image
+    .metadata() // get image metadata for size
+    .then(function (metadata) {
+      if (metadata.width > 1800) {
+        return image.resize({ width: 1800 }).toBuffer(); // resize if too big
+      } else {
+        return image.toBuffer();
+      }
+    })
+    .then(function (data) {
+      // upload to s3 storage
+      fs.rmSync(req.file.path, { force: true }); // delete the tmp file as now have buffer
+      let upload = {
+        Key: `uploads/${
+          (req.file.originalname.replace(/\W|jpeg|jpg|png/g, ""), "")
+        }.${file.extension}`, // removes non-word characters from filename
+        Body: data,
+        Bucket: "some-bucket",
+        ACL: "public-read",
+        ContentType: req.file.mimetype, // the image type
+      };
+      s3.upload(upload, function (err, response) {
+        if (err) {
+          return res
+            .status(422)
+            .send(
+              "There was an error uploading an image to s3: " + err.message
+            );
+        } else {
+          res.send(response.Location); // send the url to the stored file
+        }
+      });
+    })
+    .catch(function (err) {
+      return res
+        .status(422)
+        .send("There was an error processing an image: " + err.message);
+    });
+});
 // app.post("/api/upload", upload.single("image"), (req, res) => {
 // if (!req.file) {
 //   return res.status(400).json({ error: "No file uploaded" });
@@ -82,31 +142,31 @@ mongoose
 //   res.json({ image: uploadedImage });
 // });
 
-app.put("/api/upload", async (req, res) => {
-  //let filename = req.path.slice(1);
-  if (!req.file) {
-    return res.status(400).json({ error: "No file uploaded" });
-  }
+// app.put("/api/upload", async (req, res) => {
+//   //let filename = req.path.slice(1);
+//   if (!req.file) {
+//     return res.status(400).json({ error: "No file uploaded" });
+//   }
 
-  // Extract the file extension from the original file name
-  const fileExtension = path.extname(req.file.originalname);
+//   // Extract the file extension from the original file name
+//   const fileExtension = path.extname(req.file.originalname);
 
-  // Generate a new file name with the added extension
-  const newFileName = `${req.file.filename}${fileExtension}`;
+//   // Generate a new file name with the added extension
+//   const newFileName = `${req.file.filename}${fileExtension}`;
 
-  console.log(typeof req.body);
+//   console.log(typeof req.body);
 
-  await s3
-    .putObject({
-      Body: JSON.stringify(req.body),
-      Bucket: process.env.BUCKET,
-      Key: newFileName,
-    })
-    .promise();
+//   await s3
+//     .putObject({
+//       Body: JSON.stringify(req.body),
+//       Bucket: process.env.BUCKET,
+//       Key: newFileName,
+//     })
+//     .promise();
 
-  res.set("Content-type", "text/plain");
-  res.send("ok").end();
-});
+//   res.set("Content-type", "text/plain");
+//   res.send("ok").end();
+// });
 
 app.get("/api/data", (req, res) => {
   JsonData.find()
